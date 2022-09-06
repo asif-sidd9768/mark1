598651f38fb4b49b155b77ad57240a729a53681a
asif-sidd9768 <siddiqueasif421@gmail.com>
2022-09-06 14:20:30 +0530

    added files

---
+++ questions.js
@@ -0,0 +1,16 @@
+const questions = [
+  {
+    text: "What's my favorite color?",
+    answer: "black"
+  },
+  {
+    text: "Do I like movies? (yes or no)",
+    answer: "no"
+  },
+  {
+    text: "Where do I stay?",
+    answer: "mumbai"
+  }
+]
+
+module.exports = questions;
